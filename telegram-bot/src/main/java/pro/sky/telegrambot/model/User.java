c78385e69a28eaf70e260c7811892fbc75102693
el-kg <kirillgalkin8@gmail.com>
2024-02-01 19:14:08 +0700

    -Написал UserController -Написал методы в UserServiceImpl -Сделал javadoc описание в UserService(и один метод в UserServiceImpl) -Написал метод findByTelegramId в UserRepository -Сделал package Exception для исключений:    Написал AlreadyExistsException и NotFoundException    Сделал Exception Handler -Походу дела подключил какую-то зависимость в pom.xml( что имено не помню)

--- telegram-bot/src/main/java/pro/sky/telegrambot/model/User.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/model/User.java
@@ -34,4 +34,52 @@ public class User  {
         this.lastName = lastName;
         this.phone = phone;
     }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPetType() {
+        return PetType;
+    }
+
+    public void setPetType(String petType) {
+        PetType = petType;
+    }
+
+    public String getPetName() {
+        return PetName;
+    }
+
+    public void setPetName(String petName) {
+        PetName = petName;
+    }
 }
