325ef8b541d6a6e660655704916c8379235ef013
Ксения <korzhuk.xeniya@yandex.ru>
2024-02-17 19:20:13 +0300

    Initial commit

--- telegram-bot/src/main/java/pro/sky/telegrambot/controller/VolunteerController.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/controller/VolunteerController.java
@@ -14,6 +14,7 @@ import pro.sky.telegrambot.service.VolunteerService;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/volunteer")
 public class VolunteerController {
