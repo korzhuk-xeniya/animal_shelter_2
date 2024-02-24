319a7ac8e858318aa558bf00d1b04dfdf4703e10
glinka <alexandraglinka@yandex.ru>
2024-02-24 12:52:51 +0300

    правки исключений - удалены лишние

--- telegram-bot/src/main/java/pro/sky/telegrambot/exceptions/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package pro.sky.telegrambot.exceptions;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
